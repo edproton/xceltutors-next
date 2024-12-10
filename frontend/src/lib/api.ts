4ea97f0647c9ed64ddc5d315c4ff39ab9be53bfc
edproton <eduardscript@protonmail.com>
2024-12-10 20:27:24 +0000

    add pagination to bookings API; implement query parameters for page and limit in bookings retrieval

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,3 +4,12 @@ import { type ApiRoutes } from "@shared";
 const client = hc<ApiRoutes>(import.meta.env.VITE_API_BASE_URL);
 
 export const api = client.api.v1;
+api.bookings.$get({
+  query: {
+    page: 1,
+    limit: 10,
+  },
+}).then(async (res) => {
+  const data = await res.json();
+  // const data: any -> error 😪
+});
